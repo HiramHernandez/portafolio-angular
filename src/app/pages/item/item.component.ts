9fe702a214e82094deb122c53a26ab52b33e00f8
HiramHernandez <hhg.mzt@gmail.com>
2023-04-11 17:38:55 -0600

    fin de la sección 7

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductoService } from 'src/app/services/producto.service';
+import { IProductoDescripcion } from 'src/app/interfaces/produc-description.interface';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
-export class ItemComponent {
+export class ItemComponent implements OnInit {
+  producto: IProductoDescripcion = {};
+  id: string = "";
 
+  constructor(private route: ActivatedRoute,
+            public productpService: ProductoService) {
+  }
+
+  ngOnInit(): void {
+    this.route.params
+      .subscribe( parametros => {
+        this.productpService.getProducto(parametros['id'])
+          .subscribe(product => {
+            this.id = parametros['id'];
+            console.log(product);
+            this.producto = product;
+          });
+
+      })
+  }
 }
--- src/app/services/producto.service.ts
+++ src/app/services/producto.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { IProducto } from '../interfaces/product.interface';
+import { IProducto } from '../interfaces/producto.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +8,7 @@ import { IProducto } from '../interfaces/product.interface';
 export class ProductoService {
   productos?: IProducto[] = [];
   cargando: boolean = true;
+  productosFiltrado?: IProducto[] = [];
 
   constructor(private http: HttpClient)
   {
@@ -16,11 +17,60 @@ export class ProductoService {
 
   private cargarProductos()
   {
-    this.http.get("https://tienda-puente-main-web-app-default-rtdb.firebaseio.com/producto_idx.json")
+
+    return new Promise<void>( (resolve, reject) => {
+      this.http.get("https://tienda-puente-main-web-app-default-rtdb.firebaseio.com/producto_idx.json")
       .subscribe( (resp: any) => {
         this.productos = resp;
         this.cargando = false;
+        resolve();
       });
-  } 
+    });
+    
+  }
+
+  getProducto(id: string){
+    let url: string = `https://tienda-puente-main-web-app-default-rtdb.firebaseio.com/producto/${id}.json`;
+    return this.http.get(url);
+  }
+
+  buscarProducto(termino: string){
+    if (this.productos?.length === 0)
+    {
+      //carga productos
+      this.cargarProductos().then(() => {
+        //ejecutar después de tener los productos
+        //apliar filtro
+        this.filtrarProductos(termino);
+      })
+    }
+    else{
+
+    }
+    this.productosFiltrado = this.productos?.filter(producto => {
+      return true;
+    });
+  }
+
+  private filtrarProductos(termino: string){
+ 
+    this.productosFiltrado = [];
+    termino = termino.toLowerCase();
+
+    this.productos?.forEach( producto => {
+      if(producto.categoria || producto.titulo){
+        const tituloLowerCase = producto.titulo?.toLocaleLowerCase();
+        if (
+            (producto.categoria && producto.categoria?.indexOf( termino ) >= 0) || 
+            (tituloLowerCase && tituloLowerCase.indexOf( termino ) >= 0))
+        {
+          this.productosFiltrado?.push(producto)
+        }
+      }
+        
+      
+    });
+
+  }
 
 }
